f788e21ba0bdd5208da97f7f8704fd3139201d3c
Eric Chen <ejc196@gmail.com>
2021-03-15 21:18:55 -0700

    Added alternative pickup key

--- Escape Room/Assets/Scripts/PickUp.cs
+++ Escape Room/Assets/Scripts/PickUp.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    public Transform player;
     public Transform theDestination;
+    bool hasPlayer = false;
+    private bool holding = false;
 
     void OnMouseDown(){
         GetComponent<Rigidbody>().useGravity = false;
@@ -17,5 +20,39 @@ public class PickUp : MonoBehaviour
         this.transform.parent = null;
         GetComponent<Rigidbody>().useGravity = true;
 
+    }
+
+    void Update(){
+
+
+        //Added additional way to pickup objects, via key press E.
+        //TODO look into ways to prevent grabbing multiple objects by using raycasting.
+
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        if(dist <= 7.5f){
+            hasPlayer = true;
+        } else {
+            hasPlayer = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && hasPlayer){
+            Debug.Log("E key was pressed");
+            
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        this.transform.position = theDestination.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+        holding = true;
+        }
+        
+
+
+        if(Input.GetKeyUp(KeyCode.E)){
+            Debug.Log("E key was released");
+            
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 }
