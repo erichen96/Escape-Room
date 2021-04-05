86fb8d21f87d8e4f5a92291b767b7e9884f46529
Eric Chen <ejc196@gmail.com>
2021-04-05 12:55:07 -0700

    Recieved Assets

---
+++ Escape Room/Assets/Scripts/TeleportOnContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportOnContact : MonoBehaviour
+{
+    [SerializeField] 
+    Transform TeleportDestination;
+    [SerializeField]
+    GameObject Player;
+    public void OnTriggerEnter(Collider collision){
+        if(collision.gameObject.tag == "TeleportPlatform"){
+            Player.GetComponent<CharacterController>().enabled = false;
+            Player.transform.position = TeleportDestination.transform.position;
+            Debug.Log("Teleporting");
+            Player.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+}
