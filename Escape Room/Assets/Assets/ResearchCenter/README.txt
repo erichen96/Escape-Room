7af4cd8c2167792674265536e8514984ff10165e
Eric Chen <ejc196@gmail.com>
2021-04-14 10:28:24 -0700

    Added Ground Tags to First Room

---
+++ Escape Room/Assets/Assets/ResearchCenter/README.txt
@@ -0,0 +1,9 @@
+Thanks for purchasing Modular Research Center.
+
+To make showcase level look correctly, add Post Processing Stack to your project from Window -> Package Manager.
+Set correct Post Process profile in Post-process Volume (for Gamma or Linear color spaces).
+You can change color space in Edit->Project Settings->Player menu.
+Make sure that you have PostProcessing layer (Layers -> Edit Layers - User Layer 8). 
+In Camera settings use Deferred Rendering Path with MSAA turn off, HDR turn off.
+Use Temporal AA to get the best results (Can be set in Post Processing Profile).
+If you have any issues, please contact me via support email: air.kelheor@gmail.com
---
+++ Escape Room/Assets/Scripts/PanelAnswerHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelAnswerHandler : MonoBehaviour
+{
+    [SerializeField]
+    public Sprite[] spriteList;
+    [SerializeField]
+    public int currentImageIndex = 0;
+    [SerializeField]
+    public GameObject panel;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    // }
+
+    public void UpdateImage(){
+        currentImageIndex++;
+
+        if(currentImageIndex > spriteList.Length - 1){
+            currentImageIndex = 0;
+        }
+
+
+        panel.GetComponent<Image>().sprite = spriteList[currentImageIndex];
+    }
+}
